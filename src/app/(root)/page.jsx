f3bbd249d72c8665f90a834efe3f30f64a31dda9
Ahammad Abdullah <ahammadabdullahnew@gmail.com>
2024-02-21 03:24:55 +0600

    choose your plan section complete

--- src/app/(root)/page.jsx
+++ src/app/(root)/page.jsx
@@ -1,13 +1,15 @@
 import AllGigs from "@/Components/AllGigs/AllGigs";
+import ChoosePlans from "@/Components/ChoosePlans/ChoosePlans";
 import HomeBanner from "@/Components/sections/Banners/HomeBanner";
 
 const HomePage = () => {
-    return (
-        <>
-            <HomeBanner />
-            <AllGigs />
-        </>
-    );
+  return (
+    <>
+      <HomeBanner />
+      <AllGigs />
+      <ChoosePlans />
+    </>
+  );
 };
 
 export default HomePage;
