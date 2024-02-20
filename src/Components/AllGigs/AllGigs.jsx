48db3fbb2a90183e97c511d04bcf7981ecf32049
Sumitra Paul <sumitra.cse.just2020@gmail.com>
2024-02-20 22:40:22 +0600

    all gigs add

---
+++ src/Components/AllGigs/AllGigs.jsx
@@ -0,0 +1,67 @@
+/* eslint-disable @next/next/no-img-element */
+"use client";
+import { useEffect, useState } from "react";
+
+const AllGigs = () => {
+  const [gigs, setGigs] = useState([]);
+  const [fullDescription, setFullDescription] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("/db.json");
+      const data = await res.json();
+      setGigs(data);
+    };
+    fetchData();
+  }, []);
+
+  const handleDescription = (id) => {
+    if (fullDescription.includes(id)) {
+      setFullDescription(fullDescription.filter((descId) => descId !== id));
+    } else {
+      setFullDescription([...fullDescription, id]);
+    }
+  };
+
+  return (
+    <div>
+      <h1 className="mt-12 text-center text-4xl font-bold">
+        Popular <span className="text-green-700">Gigs</span>
+      </h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 max-w-7xl mx-auto">
+        {gigs.map((gig) => (
+          <div key={gig.id}>
+            <div className="py-10 mb-16">
+              <div
+                className={`rounded-xl overflow-hidden max-w-sm shadow-lg border border-yellow-400 ${
+                  fullDescription.includes(gig.id) ? "h-auto" : "h-[420px]"
+                }`}
+              >
+                <img className="w-full h-[200px]" src={gig.image} alt="gig" />
+                <div className="px-6 py-4">
+                  <div className="font-bold text-xl mb-2">{gig.title}</div>
+                  <p>
+                    {fullDescription.includes(gig.id)
+                      ? gig.description
+                      : gig.description.slice(0, 110)}
+                  </p>
+                  {gig.description.length > 100 && (
+                    <button
+                      className="btn btn-primary mt-2"
+                      onClick={() => handleDescription(gig.id)}
+                    >
+                      {fullDescription.includes(gig.id)
+                        ? "Show Less"
+                        : "Show More"}
+                    </button>
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default AllGigs;
--- src/app/(root)/page.jsx
+++ src/app/(root)/page.jsx
@@ -1,12 +1,11 @@
+import AllGigs from "@/Components/AllGigs/AllGigs";
 import HomeBanner from "@/Components/sections/Banners/HomeBanner";
 
 const HomePage = () => {
     return (
         <>
             <HomeBanner />
-
-
-
+            <AllGigs />
         </>
     );
 };
