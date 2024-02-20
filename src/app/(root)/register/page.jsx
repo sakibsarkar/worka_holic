adf4a278a5f7f3e8f767c1d791285d558ad5920d
binitaepa <binitaepa@gmail.com>
2024-02-21 03:47:04 +0600

    button for register is created

--- src/app/(root)/register/page.jsx
+++ src/app/(root)/register/page.jsx
@@ -1,7 +1,8 @@
 const Register = () => {
     return (
         <div>
-            Register
+            <div className="flex items-center justify-center  gap-10"><button className="bg-gray-300 rounded-none p-3 btn btn-success ">Candidate</button>
+            <button className="bg-[#007456] text-white flex  rounded-none p-3 justify-center items-center text-[#007456s]">Employer</button></div>
         </div>
     );
 };
